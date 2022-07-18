5a0f551ea8f6b47bc2e1d8a865bff221989038ac
Ryan Murphy <TheDecoder007@gmail.com>
2022-07-18 18:45:16 -0400

    began comment routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,10 @@ const pizzaRoutes = require('./pizza-routes');
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
 
+const commentRoutes = require('./comment-routes');
+const pizzaRoutes = require('./pizza-routes');
 
+router.use('/comments', commentRoutes);
+router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
